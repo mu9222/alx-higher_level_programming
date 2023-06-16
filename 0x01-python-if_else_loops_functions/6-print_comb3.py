8665f4a9662cdbcb10c658544dc30d9f8541c8c3
mu9222 <eng92fa64@gmail.com>
2023-06-16 00:33:31 +0000

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for digit1 in range(0, 10):
+    for digit2 in range(0, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        elif digit2 > digit1:
+            print("{}{}".format(digit1, digit2), end=", ")
