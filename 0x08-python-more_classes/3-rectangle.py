f7a4411cdd6452338aa9a432ca55c16936d677a1
mu9222 <eng92fa64@gmail.com>
2023-07-08 11:00:19 +0000

    Task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -49,6 +49,7 @@ class Rectangle:
 
     def __str__(self):
         """ Return the printable representation of the Rectangle.
+
             Represents the rectangle with the # character.
         """
         if self.__width == 0 or self.__height == 0:
